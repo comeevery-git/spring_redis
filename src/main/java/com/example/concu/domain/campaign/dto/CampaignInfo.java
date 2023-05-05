2a053e042ce1d7f73f388e0be7d59779115fa5ed
lydia <lydia.s.kang@nujackpot.com>
2023-05-05 23:51:48 +0900

    feat: Redis Cache 적용, campaign 목록 조회 API 에서 사용

--- src/main/java/com/example/concu/domain/campaign/dto/CampaignInfo.java
+++ src/main/java/com/example/concu/domain/campaign/dto/CampaignInfo.java
@@ -3,9 +3,11 @@ package com.example.concu.domain.campaign.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Builder
 @Getter
-public class CampaignInfo {
+public class CampaignInfo implements Serializable {
     Long campaignId;
     Long maxApplierCount;
 
